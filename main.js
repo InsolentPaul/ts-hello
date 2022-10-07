8cd97c2b38b921aca3e41219b7244b663add4887
InsolentPaul <pavlo.kravchenko@proton.me>
2022-10-07 20:10:37 +0300

    variable types + enum

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log(i + "finnaly");
+/* function doSomething() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  console.log(i + "finnaly");
 }
-doSomething();
+
+doSomething(); */
+var count = 5;
+// count = "a"; // error because count is number
+var x; // no error because x has any value
+x = "a";
+x = 1;
+x = true;
+var y; // only numbers can be assigned, other will be error
+var y1;
+var y2;
+var y3;
+var yArray = [1, 2, 3]; // only set by type (numbers) in array
+var colorRed = 0;
+var colorGreen = 1;
+var colorBlue = 2;
+var color;
+(function (color) {
+    color[color["Red"] = 0] = "Red";
+    color[color["Green"] = 1] = "Green";
+    color[color["Blue"] = 2] = "Blue";
+})(color || (color = {}));
+var backgroundColor = color.Red;
--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
-function doSomething() {
+/* function doSomething() {
   for (var i = 0; i < 5; i++) {
     console.log(i);
   }
   console.log(i + "finnaly");
 }
 
-doSomething();
+doSomething(); */
+
+let count = 5;
+// count = "a"; // error because count is number
+
+let x; // no error because x has any value
+x = "a";
+x = 1;
+x = true;
+
+let y: number; // only numbers can be assigned, other will be error
+let y1: boolean;
+let y2: string;
+let y3: any;
+
+let yArray: number[] = [1, 2, 3]; // only set by type (numbers) in array
+
+const colorRed = 0;
+const colorGreen = 1;
+const colorBlue = 2;
+
+enum color {
+  Red = 0,
+  Green = 1,
+  Blue = 2,
+}
+
+let backgroundColor = color.Red;
